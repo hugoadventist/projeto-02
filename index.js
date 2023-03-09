2d2ce84bd047a07779d8b0d3b0c902f13af034ea
hugoadventist <28987448+hugoadventist@users.noreply.github.com>
2023-03-09 04:17:36 -0300

    Added js script...

---
+++ index.js
@@ -0,0 +1,13 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let turns = document.querySelector('.turns');
+let latestResult = document.querySelector('.latestResult');
+let highOrLow = document.querySelector('.highOrLow')
+let sendTurn = document.querySelector('.sendTurn')
+let turnField = document.querySelector('.turnField');
+
+let turnCount = 1;
+let restartButton;
+
+let checkTurn = () => {
+  alert('Eu sou um placeholder');
+}
